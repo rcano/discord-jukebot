74646cb46fc82534ea4df962b87e9adfb0d6af05
rcano <ioniviil@gmail.com>
2016-10-05 15:35:50 -0300

    Fixed bug preventing messages from being sent.
    And added a bit of stdout logging to that.

--- src/main/scala/jukebox/DiscordRateHonoringSender.scala
+++ src/main/scala/jukebox/DiscordRateHonoringSender.scala
@@ -17,47 +17,53 @@ class DiscordRateHonoringSender(val exceptionLogger: Throwable => Unit = null) {
   private val pendingMessages = new java.util.HashMap[(IChannel, Option[IUser]), java.util.LinkedList[(String, Promise[Unit])]]().asScala.withDefaultValue(new java.util.LinkedList)
   scheduler.scheduleWithFixedDelay(new Runnable {
       def run = pendingMessages.synchronized {
+        if (pendingMessages.nonEmpty) println("pending messages:\n  " + pendingMessages.map(e => (e._1._1.getName -> e._1._2.map(_.getName)) + " -> " + e._2.size).mkString("\n  "))
         pendingMessages.headOption foreach {
           case ((channel, author), msgs) =>
-            val sizes = msgs.asScala.map(_._1.length)
-
-            @inline @tailrec def clampMessages(acc: Int = 0, i: Int = 0): (Int, Int) = {
+            @tailrec def clampMessages(acc: Int = 0, i: Int = 0): (Int, Int) = {
               val newAcc = msgs.get(i)._1.length + acc
               if (newAcc > MaxSize) (acc, i) //reached cutoff
+              else if ((i + 1) == msgs.size) (newAcc, i)
               else clampMessages(newAcc, i + 1)
             }
-            val (targetSize, lastIndexToSend) = clampMessages()
-            if (targetSize == 0) { //first message is too large, have to split it up
-              val (msg, promise) = msgs.remove(0)
-              val (targetMsg, rest) = msg.splitAt(MaxSize)
-              try {
-                channel.sendMessage(targetMsg)
-                msgs.addFirst((rest, promise))
-              } catch {
-                case e: RateLimitException => //do nothing, will reattempt the next cycle
-                case NonFatal(e) => 
-                  if (exceptionLogger != null) exceptionLogger(e)
-                  promise.failure(e)
-              }
-            } else {
-              val (finalMsg, promises) = (0 to lastIndexToSend).foldLeft((new StringBuilder, Vector.empty[Promise[Unit]])) {
-                case ((sb, promises), i) =>
-                  val (msg, promise) = msgs.get(i)
-                  sb.append(msg).append("\n") -> (promises :+ promise)
-              }
-              try {
-                channel.sendMessage(finalMsg.toString)
-                promises foreach (_.success(()))
-                (0 to lastIndexToSend) foreach (_ => msgs.removeFirst)
-              } catch {
-                case e: RateLimitException => //do nothing, will reattempt the next cycle
-                case NonFatal(e) =>
-                  if (exceptionLogger != null) exceptionLogger(e)
-                  promises foreach (_.failure(e))
+            try {
+
+              val (targetSize, lastIndexToSend) = clampMessages()
+              println("sending " + (targetSize -> lastIndexToSend))
+              if (targetSize == 0) { //first message is too large, have to split it up
+                val (msg, promise) = msgs.remove(0)
+                val (targetMsg, rest) = msg.splitAt(MaxSize)
+                try {
+                  channel.sendMessage(targetMsg)
+                  msgs.addFirst((rest, promise))
+                } catch {
+                  case e: RateLimitException => //do nothing, will reattempt the next cycle
+                  case NonFatal(e) =>
+                    if (exceptionLogger != null) exceptionLogger(e)
+                    promise.failure(e)
+                }
+              } else {
+                val (finalMsg, promises) = (0 to lastIndexToSend).foldLeft((new StringBuilder, Vector.empty[Promise[Unit]])) {
+                  case ((sb, promises), i) =>
+                    val (msg, promise) = msgs.get(i)
+                    sb.append(msg).append("\n") -> (promises :+ promise)
+                }
+                try {
+                  channel.sendMessage(finalMsg.toString)
+                  promises foreach (_.success(()))
                   (0 to lastIndexToSend) foreach (_ => msgs.removeFirst)
+                } catch {
+                  case e: RateLimitException => //do nothing, will reattempt the next cycle
+                  case NonFatal(e) =>
+                    if (exceptionLogger != null) exceptionLogger(e)
+                    promises foreach (_.failure(e))
+                    (0 to lastIndexToSend) foreach (_ => msgs.removeFirst)
+                }
               }
+              if (msgs.isEmpty) pendingMessages.remove((channel, author))
+            } catch {
+              case NonFatal(e) => if (exceptionLogger != null) exceptionLogger(e)
             }
-            if (msgs.isEmpty) pendingMessages.remove((channel, author))
         }
       }
     }, 300, 300, MILLISECONDS)
